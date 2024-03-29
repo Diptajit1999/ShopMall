c65a421a29606f70b37dcc290ce2ea5f5b9442fe
Diptajit Das <diptajitdas66@gmail.com>
2024-03-29 23:59:27 +0530

    writting logic for file Uploads

--- server/multerConfig/admin/admin.storageConfig.js
+++ server/multerConfig/admin/admin.storageConfig.js
@@ -1,9 +1,24 @@
 const multer=require("multer")
 
 
-
+// storage config
 const storage=multer.diskStorage({
     destination:(req,file,callback)=>{
         callback(null,"./adminUploads")
+    },
+    filename:(req,file,callback)=>{
+        const filename=`image-${Date.now()}.${file.originalname}`
+        callback(null,filename)
     }
 })
+
+
+// filter
+const filefilter=(req,file,callback)=>{
+    if(file.mimetype==="image/png"||file.mimetype==="image/jpg"||file.mimetype==="image/jpeg"){
+        callback(null,true)
+    }else{
+        callback(null,false)
+        return callback(new Error("Only give jpg,jpeg and png"))
+    }
+}
